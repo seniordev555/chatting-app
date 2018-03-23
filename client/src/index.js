7185f337ab4350dcf49f2dcdda26732169cb3090
seniordev555 <seniordev555@gmail.com>
2018-03-23 05:07:07 +0200

    initial create react app

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './store';
-import App from './wrappers/App';
+import './index.scss';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-// Styles
-// Import Main styles for this application
-import '../scss/style.scss'
-// Temp fix for reactstrap
-import '../scss/core/_dropdown-menu-right.scss'
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-if (process.env.NODE_ENV === 'development') {
-  require('babel-register');
-  require('babel-polyfill');
-
-  require('./server');
-}
+require('./server');
